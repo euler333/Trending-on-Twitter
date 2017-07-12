0e9fa8bf86ab87e94e76952ec0b31d61b97aeaa8
Jacob Cohen <jacob.n.cohen@hotmail.com>
2017-07-12 15:45:00 +0000

    code corrected, modernized, modularized and modified to only return Twitter API response

--- Twitter/authorization.js
+++ Twitter/authorization.js
@@ -1,27 +1,24 @@
-var https = require('https'),
-oauthJsonFile = require('fs').createWriteStream('oauth.json');
-var gettingAccess = {
-  consumerKey: '<Your_Consumer_Key>',
-  consumerSecretKey: '<Your_Consumer_Secret_Key>' 
+exports.authorize = () => {
+    var https = require('https'),
+        oauthJsonFile = require('fs').createWriteStream('/home/ubuntu/workspace/app/Twitter/oauth.json'),
+        gettingAccess = {
+            consumerKey: 'lPMNYMa4sgP2UR7RvFMQZqDZT',
+            consumerSecretKey: 'NPHeGLtNkfikwtp7d1kr3fVnKlvMdgLpX579BcaFXsSxfmyYZL' 
+        },
+        request = https.request({
+            method: 'POST',
+            host: 'api.twitter.com',
+            path: '/oauth2/token',
+            headers: {
+                'User-Agent': 'Coding Defined',
+                Authorization: 'Basic ' + Buffer((encodeURIComponent(gettingAccess.consumerKey) + ':' + encodeURIComponent(gettingAccess.consumerSecretKey))).toString('base64'),
+                'Content-type': 'application/x-www-form-urlencoded;charset=UTF-8',
+                'Content-length': 29
+            }
+        });
+    request.end('grant_type=client_credentials');
+    request.on('response', response => {
+        if (response.statusCode !== 200) return console.error(response.statusCode);
+        response.pipe(oauthJsonFile);
+    });
 };
-
-var request = https.request({
-  method: 'POST',
-  host: 'api.twitter.com',
-  path: '/oauth2/token',
-  headers: {
-    'User-Agent': 'Coding Defined',
-    Authorization: 'Basic ' + Buffer((encodeURIComponent(gettingAccess.consumerKey) + ':' + encodeURIComponent(gettingAccess.consumerSecretKey))).toString('base64'),
-    'Content-type': 'application/x-www-form-urlencoded;charset=UTF-8',
-    'Content-length': 29
-  }
-});
-
-request.end('grant_type=client_credentials');
-
-request.on('response', function(response) {
-  if(response.statusCode !== 200) {
-    return console.log('Error ' + response.statusCode);
-  }
-  response.pipe(oauthJsonFile);
-});
--- Twitter/trends.js
+++ Twitter/trends.js
@@ -1,61 +1,30 @@
-// Author Coding Defined
-
-var https = require('https');
-var headers = {
-	'User-Agent': 'Coding Defined',
-	Authorization: 'Bearer ' + require('./oauth.json').access_token
-};
-
-function callTwitter(options, callback){
-	https.get(options, function(response) {
-		jsonHandler(response, callback);
-	}).on('error', function(e) {
-		console.log('Error : ' + e.message);
-	})
-}
-
-var trendOptions = {
-	host: 'api.twitter.com',
-	path: '/1.1/trends/place.json?id=1', // id = 1 for global trends
-	headers: headers
-}
-
-var tweetDetails = {
-	maxresults: 10,
-	resultType: 'recent', // options are mixed, popular and recent
-	options: {
-		host: 'api.twitter.com',
-		headers: headers,
-	}
-}
-
-function jsonHandler(response, callback) {
-	var json = '';
-	response.setEncoding('utf8');
-	if(response.statusCode === 200) {
-		response.on('data', function(chunk) {
-			json += chunk;
-		}).on('end', function() {
-			callback(JSON.parse(json));
+var https = require('https'),
+	headers = {
+		'User-Agent': 'Coding Defined',
+		Authorization: 'Bearer ' + require('./oauth.json').access_token
+	},
+	callTwitter = (options, callback) => {
+		https.get(options, response => {
+			jsonHandler(response, callback);
+		}).on('error', e => {
+			console.error(e.message);
 		});
-	} else {
-		console.log('Error : ' + reseponse.statusCode);
-	}
-}
-
-function fullTweetPath(query) {
-	var path = '/1.1/search/tweets.json?q=' + query
-	+ '&count=' + tweetDetails.maxResult
-	+ '&include_entities=true&result_type=' + tweetDetails.resultType;
-
-	tweetDetails.options.path = path; 
-}
-
-callTwitter(trendOptions, function(trendsArray) {
-	fullTweetPath(trendsArray[0].trends.query)
-	callTwitter(tweetDetails.options, function(tweetObj) {
-		tweetObj.statuses.forEach(function(tweet) {
-			console.log('\n' + tweet.user.screen_name + ' : ' + tweet.text);
-		})
-	})
-});
+	},
+	trendOptions = {
+		host: 'api.twitter.com',
+		path: '/1.1/trends/place.json?id=1', // id = 1 for global trends
+		headers: headers
+	},
+	jsonHandler = (response, callback) => {
+		var json = '';
+		response.setEncoding('utf8');
+		if (response.statusCode === 200) {
+			response.on('data', chunk => {
+				json += chunk;
+			}).on('end', () => {
+				callback(JSON.parse(json));
+			});
+		} else console.error(response.statusCode);
+	};
+exports.callTwitter = callTwitter;
+exports.trendOptions = trendOptions;
