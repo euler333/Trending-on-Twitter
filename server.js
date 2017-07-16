3be0c967fb16047e9cb737d59d36582c93031a2a
Jacob Cohen <jacob.n.cohen@hotmail.com>
2017-07-16 18:58:38 +0000

    modified /currentTrends styling

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ app.get('/currentTrends', (req, res) => {
                     <style>
                         *{
                             box-sizing: border-box;
+                            font-weight: bold;
+                            font-family: Verdana;                            
                         }
                         #graph{
                             display: inline-flex;
@@ -20,17 +22,19 @@ app.get('/currentTrends', (req, res) => {
                         #topics{
                             margin: 0px 10px 0px 0px;
                         }
-                        dt{
+                        li{
                             height: 44px;
                             padding-top: 11px;
-                            font-weight: bold;
-                            font-family: Verdana;
+                        }
+                        h1{
+                            font-size: 60px;
                         }
                     </style>
                 </head>
                 <body>
+                    <h1 align="center">Trending on Twitter</h1>
                     <div id="graph">
-                        <dl id="topics"></dl>
+                        <ol id="topics"></ol>
                     </div>
                     <script>
                         (() => {
@@ -47,7 +51,7 @@ app.get('/currentTrends', (req, res) => {
                                                         window.webkitRequestAnimationFrame || 
                                                         window.msRequestAnimationFrame;                                
                             trends.forEach(trend => {
-                                var topic=document.createElement('dt');
+                                var topic=document.createElement('li');
                                 topic.innerHTML=trend.name;
                                 topics.appendChild(topic);
                                 widths.push(0);
@@ -60,42 +64,52 @@ app.get('/currentTrends', (req, res) => {
                             function draw() {
                                 context.clearRect(0, 0, canvas.width, canvas.height);
                                 for (let i=0; i<widths.length; i++){ 
+                                    
                                     //draw bars
                                     context.fillStyle='blue';
                                     context.beginPath();
                                     context.fillRect(0, 1+44*i, widths[i], 42);
-                                    context.closePath();                                    
                                     context.fill();
+                                    context.closePath();                                    
                                     
                                     //draw text
-                                    let text = Math.round(trends[i].tweet_volume*widths[i]/maxWidths[i]),
-                                        textWidth = context.measureText(text).width + 5;                                    
+                                    let text=Math.round(trends[i].tweet_volume*widths[i]/maxWidths[i]);
                                     context.fillStyle='black';
                                     context.beginPath();
-                                    context.font = '20px arial';
-                                    context.fillText(text, widths[i]+15, 30+44*i);
-                                    context.closePath();                                    
-                                    context.fill();                                    
+                                    context.font='20px arial';
+                                    context.fillText(text, widths[i]+25, 30+44*i);
+                                    context.fill();
                                     
                                     //draw tweet volume content box
-                                    let x = widths[i]+3,
-                                        y = 22.5+44*i,
-                                        width = textWidth;
+                                    let width=context.measureText(text).width + 22; 
+                                        height=42,
+                                        x=widths[i]+15,
+                                        y=(height+2)*i+7,
+                                        cornerRadius=5,
+                                        lineWidth=2;
                                     context.beginPath();
                                     context.moveTo(x, y);    
-                                    context.lineTo(x+=5, y+=5);    
-                                    context.lineTo(x, y+=5);
-                                    context.arc(x+=5, y+=5, 5, Math.PI, Math.PI/2, true);
-                                    context.lineTo(x+=width, y+=5);
-                                    context.arc(x, y-=5, 5, Math.PI/2, 0, true);
-                                    context.lineTo(x+=5, y-=30);
-                                    context.arc(x-=5, y, 5, 0, -Math.PI/2, true);
-                                    context.arc(x-=width, y, 5, -Math.PI/2, Math.PI, true);
-                                    context.lineTo(x-=5, y+=10);
-                                    context.lineTo(x-=5, y+=5);
-                                    context.lineWidth = 2;
+                                    context.lineTo(x, y+=(height-2*cornerRadius-lineWidth)); 
+                                    context.arc(x+=cornerRadius, y, cornerRadius, Math.PI, Math.PI/2, true);
+                                    context.lineTo(x+=(width-2*cornerRadius-lineWidth), y+=cornerRadius);
+                                    context.arc(x, y-=cornerRadius, cornerRadius, Math.PI/2, 0, true);  
+                                    context.lineTo(x+=cornerRadius, y-=(height-2*cornerRadius-lineWidth));
+                                    context.arc(x-=cornerRadius, y, cornerRadius, 0, -Math.PI/2, true);
+                                    context.lineTo(x-=(width-2*cornerRadius-lineWidth), y-=cornerRadius);
+                                    context.arc(x, y+=cornerRadius, cornerRadius, -Math.PI/2, Math.PI, true);
+                                    context.closePath();                                    
+                                    context.lineWidth=lineWidth;
+                                    context.stroke();     
+                                    
+                                    //draw arrow head
+                                    let center = y + 15;
+                                    context.beginPath();
+                                    context.moveTo(x-10, center-5);    
+                                    context.lineTo(x-15, center);
+                                    context.lineTo(x-10, center+5);
                                     context.closePath();
-                                    context.stroke();                                    
+                                    context.lineWidth=lineWidth;
+                                    context.stroke();     
                                 }
                                 for (let i=0; i<widths.length; i++){ 
                                     if (widths[i]<maxWidths[i]) widths[i]++;
